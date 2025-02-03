926b124830740365453f0b20e1f31f754d465de3
bojokoeva <bojokoeva34@gmail.com>
2025-02-02 22:56:15 -0500

    Task 2: For Loop - Sales Report

--- cc_4.js
+++ cc_4.js
@@ -6,3 +6,13 @@ if (purchaseAmount > 100) {
     purchaseAmount -= discount;
 }
 console.log(`Final amount after discount: $${purchaseAmount}`); 
+
+
+// Task 2: For Loop - Sales Report
+
+let sales = [120, 85, 200, 150, 90]; 
+let totalSales = 0;
+for (let i = 0; i < sales.length; i++) {
+    totalSales += sales[i]; // Calculate total sales
+}
+console.log(`Total sales: $${totalSales}`); 
